5328c60febd7ef77574bf045acb12b9ad5c997af
jia58960 <547221469@qq.com>
2013-10-28 22:38:58 +0800

    angularjs demo One

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,9 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('MyCtrl1', ['$scope',function($scope) {
+  	$scope.qty = 2;
+  	$scope.cost = 2.596;
 
   }])
   .controller('MyCtrl2', [function() {
