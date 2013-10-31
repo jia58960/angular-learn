149d9d7c4e635d9c488ab9d583fb808fd8768a8b
zhangjia <547221469@qq.com>
2013-10-31 09:02:56 +0800

    validate_demo

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,22 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', ['$scope','time',function($scope,time) {
+  controller('MyCtrl1', ['$scope',function($scope) {
+  	$scope.master = {};
+
+  	$scope.update = function(user){
+  		$scope.master = angular.copy(user);
+  	}
   	
-  	$scope.time = time;
+  	$scope.reset = function(){
+  		$scope.user = angular.copy($scope.master);
+  	}
+
+  	$scope.isUnchanged = function(user){
+  		return angular.equals(user, $scope.master);
+  	}
+
+  	$scope.reset();
 
   }])
   .controller('MyCtrl2', [function() {
